90ce83ab0b7dbedb8c18cadff62f0aed18e5435c
bala-amypo <bala@amypo.in>
2025-12-21 13:20:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sku;
+    private String name;
+    private boolean active = true;
+}
---
+++ src/main/java/com/example/demo/entity/Store.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+}
