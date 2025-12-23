8cdf3f7bb1bc8114ed5a09a8c940f107978c0a82
bala-amypo <bala@amypo.in>
2025-12-23 07:59:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/StoreServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.StoreService;
+import org.springframework.stereotype.Service; // Import this!
+
+@Service // Add this!
+public class StoreServiceImpl implements StoreService {
+    // constructor and methods
+}
