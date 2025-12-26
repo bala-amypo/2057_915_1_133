124f64bc83ed28cebb4fd659eeb9dbc5ddd5ddd9
bala-amypo <bala@amypo.in>
2025-12-26 05:04:16 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,37 +1,20 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    /**
-     * Configures the OpenAPI bean to define the project title and 
-     * incorporate JWT Bearer Token authentication for the Swagger UI.
-     */
     @Bean
     public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
-        
         return new OpenAPI()
-                .info(new Info()
-                        .title("Multi-Location Inventory Balancer API")
-                        .version("1.0")
-                        .description("Backend system for redistributing stock across multiple store locations."))
-                // Add Security Requirement for all API endpoints
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9084.408procr.amypo.ai/swagger-ui/index.html")
+                ));
+        }
 }
