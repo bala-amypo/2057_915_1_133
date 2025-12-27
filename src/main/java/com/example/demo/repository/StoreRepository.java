69c7834e2617434a4e480ee3e397c55c3b84e587
bala-amypo <bala@amypo.in>
2025-12-27 10:33:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/StoreRepository.java
+++ src/main/java/com/example/demo/repository/StoreRepository.java
@@ -3,8 +3,12 @@ package com.example.demo.repository;
 import com.example.demo.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
-    Store findByName(String name);
+    
+    // Change findByName to findByStoreName
+    Optional<Store> findByStoreName(String storeName);
+    
 }
